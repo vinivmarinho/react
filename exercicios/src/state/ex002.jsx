74564a94cb2a96530179697067218bee7c956251
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-01-14 11:04:33 -0400

    feat: Adiciona exercício que trabalha useState e renderização condicional

--- exercicios/src/state/ex002.jsx
+++ exercicios/src/state/ex002.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 export default function ToggleText() {
+  // Cria um estado chamado "visivel"
+  // "visivel" => Valor atual do estado (true ou false)
+  // setVisivel => Função usada para alterar o estado
+  // useState(false) => Valor inicial do estado é false
   const [visivel, setVisivel] = useState(false);
+  // Função é chamada quando o botão é clicado
   function handleClick() {
+    // Atualiza o estado para o valor oposto e depois o React renderiza o componente novamente
     setVisivel(!visivel);
   }
   return (
     <div>
-      <button onClick={handleClick}>Mostrar</button>
+      {/*Botão:
+      - onClick recebe a função "handleClick". 
+      - Usa operador ternário para o texto do botão mudar de acordo com o estado:*/}
+      <button onClick={handleClick}>{visivel ? "Esconder" : "Mostrar"}</button>
+      {/*Renderização Condicional (usa operador &&):
+      - O React avalia "visivel. Se for true, o <p> é renderizado, se não, nada aparece
+      */}
+      {visivel && <p>Esse texto só aparece se o estado "visivel" for "true"</p>}
     </div>
   );
 }
